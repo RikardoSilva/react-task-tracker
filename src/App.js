1ac0696627a06065a84cfe2d8f6d7dc3583b2bb9
RikardoSilva <ricardoporto09@yahoo.com>
2023-04-17 13:22:12 +0100

    Small updates, *not fixed*

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
+import { BrowserRouter as Switch, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
-import AddTask from "./components/AddTask";
+import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
+import About from "./components/About"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -81,11 +84,17 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show!'}
-    </div>
+    <Switch>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show!'}
+        <Footer />
+      </div>
+      <Route path='/about'>
+        <About />
+      </Route>
+    </Switch>
   )
 }
 
